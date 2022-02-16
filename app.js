228e4d2a2b9fe53e9f0392b215085ec501333d20
esvelpe <esneidervelpe@gmail.com>
2022-02-16 13:04:58 -0500

    clase 9,10,11

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ new Vue({
         return {
             name: 'Bitcoin',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent:10,
-            prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+            changePercent:-1,
+            //prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+
+            color: 'f4f4f4',
+
+            price:9000,
+
             pricesWithDays: [
                 { day: 'Lunes', value: 8400 },
                 { day: 'Martes', value: 7900 },
@@ -15,7 +20,17 @@ new Vue({
                 { day: 'Viernes', value: 9400 },
                 { day: 'Sabado', value: 10000 },
                 { day: 'Domingo', value: 10200 },
-            ]
+            ],
+
+            showPrices: true
+        }
+
+    },
+    methods: {
+        toggleShowPrices(){
+            this.showPrices=!this.showPrices
+
+            this.color=this.color.split('').reverse().join('')
         }
     }
 })
