19f353fd27dafffd7cc7e70942ed636bb61edd46
esvelpe <esneidervelpe@gmail.com>
2022-03-01 18:13:20 -0500

    Primeros componentes

--- platzi-exchange/src/components/PxHeader.vue
+++ platzi-exchange/src/components/PxHeader.vue
@@ -3,13 +3,13 @@
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <px-icon />
+          <px-icon class="mr-2" />
           <span class="font-semibold text-xl tracking-tight"
             >Platzi Exchange</span
           >
         </div>
         <div
-          class="sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
+          class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
         >
           <div class="text-sm lg:flex-grow"></div>
         </div>
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-import PxIcon from "@/components/PxIcon";
+import PxIcon from "@/components/PxIcon.vue";
 export default {
   name: "PxHeader",
   components: { PxIcon },
