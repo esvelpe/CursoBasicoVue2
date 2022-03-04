1972c1e75b80c1f6d5cdeabe06a5c1a0568f44a9
esvelpe <esneidervelpe@gmail.com>
2022-03-04 03:14:27 -0500

    Creación de router con errores

---
+++ platzi-exchange/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <px-assets-table />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable.vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+
+  components: { PxAssetsTable },
+};
+</script>
